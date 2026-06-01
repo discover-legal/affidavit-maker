4ce7fe0cea566f6ddc59bac25ae618a9529719be
hordruma <138844831+hordruma@users.noreply.github.com>
2026-06-01 18:17:34 -0400

    fix: import UserProfile from @auth0/nextjs-auth0/client (#332)

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { UserProvider } from '@auth0/nextjs-auth0/client';
-import type { UserProfile } from '@auth0/nextjs-auth0';
+import type { UserProfile } from '@auth0/nextjs-auth0/client';
 
 export default function Providers({
   children,
